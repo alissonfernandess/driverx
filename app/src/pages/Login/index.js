40e89718e549034eb92a13a111b11e38c53ed589
alissonfernandes <alisson.flutter@gmail.com>
2021-01-28 11:33:26 -0300

    criando pagina de login

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
+
+import logo from '../../assets/logo.png';
+import bgBottom from '../../assets/bg-bottom-login.png';
+
+import {Container, Button, ButtonText} from '../../styles';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Container color="info50" justify="flex-end">
+      <Container
+        justify="space-around"
+        padding={30}
+        position="absolute"
+        height={270}
+        top={0}
+        zIndex={9}>
+        <Image source={logo} />
+
+        <Button type="info">
+          <ButtonText>Fazer Login com Facebook</ButtonText>
+        </Button>
+
+        <Button type="light">
+          <ButtonText>Fazer Login com Facebook</ButtonText>
+        </Button>
+      </Container>
+
+      <Image source={bgBottom} />
+    </Container>
   );
 };
 
