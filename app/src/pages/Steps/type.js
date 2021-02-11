87217f08d15f5e5ab8b17f05a0e506042704bec8
alissonfernandes <alisson.flutter@gmail.com>
2021-02-10 21:56:49 -0300

    verificando tipo de usuario

--- app/src/pages/Steps/type.js
+++ app/src/pages/Steps/type.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Image} from 'react-native';
 
+import {useDispatch, useSelector} from 'react-redux';
+import {updateUser} from '../../store/modules/app/actions';
+
 import car from '../../assets/car.png';
 import hand from '../../assets/hand.png';
 
@@ -14,6 +17,15 @@ import {
 } from '../../styles';
 
 const Type = () => {
+  // get INITIAL_STATE user reducer
+  const {user} = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  const toogleType = (tipo) => {
+    dispatch(updateUser({tipo}));
+  };
+
   return (
     <Container padding={30} justify="flex-start">
       <Container aling="flex-start" height={40}>
@@ -22,13 +34,19 @@ const Type = () => {
       </Container>
 
       <Container>
-        <PickerButton active>
+        <PickerButton
+          onPress={() => toogleType('M')}
+          active={user.tipo === 'M'}>
           <Image source={car} />
           <Title>Motorista</Title>
         </PickerButton>
 
         <PickerButton>
-          <Image source={hand} />
+          <Image
+            source={hand}
+            onPress={() => toogleType('P')}
+            active={user.tipo === 'P'}
+          />
           <Title>Passageiro</Title>
         </PickerButton>
       </Container>
