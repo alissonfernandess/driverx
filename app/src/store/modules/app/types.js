d7d45f21446e12b2327af9466f23a996024b0c74
alissonfernandes <alisson.flutter@gmail.com>
2021-02-15 19:56:26 -0300

    create type updatecar

--- app/src/store/modules/app/types.js
+++ app/src/store/modules/app/types.js
@@ -2,6 +2,7 @@ const types = {
   UPDATE_USER: '@app/UPDATE_USER',
   CREATE_USER: '@app/CREATE_USER',
   CHECK_USER: '@app/CHECK_USER',
+  UPDATE_CAR: '@app/UPDATE_CAR',
 };
 
 export default types;
