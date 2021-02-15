11c19b56e3a470a7264fe1e081bba1847ee2026f
alissonfernandes <alisson.flutter@gmail.com>
2021-02-15 20:16:02 -0300

    criado tipo de atualizar pagamento

--- app/src/store/modules/app/types.js
+++ app/src/store/modules/app/types.js
@@ -3,6 +3,7 @@ const types = {
   CREATE_USER: '@app/CREATE_USER',
   CHECK_USER: '@app/CHECK_USER',
   UPDATE_CAR: '@app/UPDATE_CAR',
+  UPDATE_PAYMENT: '@app/UPDATE_PAYMENT',
 };
 
 export default types;
