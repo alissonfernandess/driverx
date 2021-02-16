c7232f27cab4b5ab30716e79346c675d4c531d40
alissonfernandes <alisson.flutter@gmail.com>
2021-02-15 21:23:34 -0300

    create type getrideinfos e updateride

--- app/src/store/modules/app/types.js
+++ app/src/store/modules/app/types.js
@@ -4,6 +4,8 @@ const types = {
   CHECK_USER: '@app/CHECK_USER',
   UPDATE_CAR: '@app/UPDATE_CAR',
   UPDATE_PAYMENT: '@app/UPDATE_PAYMENT',
+  GET_RIDE_INFOS: '@app/GET_RIDE_INFOS',
+  UPDATE_RIDE: '@app/UPDATE_RIDE',
 };
 
 export default types;
